335da86b35e633ed146b07bebc24076279610890
Lucas362 <lucasvitorifg@gmail.com>
2020-08-12 00:30:30 -0300

    Connecting reducer to project

--- tsreact/src/App.tsx
+++ tsreact/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import RepositoryList from './components/RepositoryList';
+
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Provider store={store}>
+
+      <div className="App">
+        <RepositoryList />
+      </div>
+    </Provider>
   );
 }
 
--- tsreact/src/components/RepositoryList/index.tsx
+++ tsreact/src/components/RepositoryList/index.tsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
-export default class RepositoryList extends Component {
+import { Repository } from '../../store/ducks/repositories/types';
+import * as repositoriesActions from '../../store/ducks/repositories/actions';
+import { ApplicationState } from '../../store';
+
+interface StateProps {
+    repositories: Repository[]
+}
+
+interface DispatchProps {
+    loadRequest(): void
+}
+
+
+type Props = StateProps & DispatchProps;
+
+class RepositoryList extends Component<Props> {
+    componentDidMount() {
+        const { loadRequest } = this.props;
+
+        loadRequest();
+    }
     render() {
+        const { repositories } = this.props;
         return (
-            <ul />
+            <ul>
+                {repositories.map(repository => <li>{repository.name}</li>)}
+            </ul>
         );
     }
 }
+
+const mapStateToProps = (state: ApplicationState) => ({
+    repositories: state.repositories.data,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => 
+    bindActionCreators(repositoriesActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(RepositoryList);
